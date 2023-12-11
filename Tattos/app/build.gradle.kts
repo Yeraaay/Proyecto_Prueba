c6cd9e7dad1e03ef753be01befed980847ce0348
Yeray Moraleda Cabrera <moraledacabrerayerai@gmail.com>
2023-12-11 02:02:33 +0100

    estética terminada

--- Tattos/app/build.gradle.kts
+++ Tattos/app/build.gradle.kts
@@ -52,4 +52,5 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+    implementation("androix.navigation:navigation-compose:2.5.2")
 }
