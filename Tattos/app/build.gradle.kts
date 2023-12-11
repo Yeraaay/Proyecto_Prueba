21f86f85253972322ded608e7ccfae3805eab3d2
Yeray Moraleda Cabrera <moraledacabrerayerai@gmail.com>
2023-12-11 02:08:13 +0100

    navegation fuera

--- Tattos/app/build.gradle.kts
+++ Tattos/app/build.gradle.kts
@@ -52,5 +52,4 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
-    implementation("androix.navigation:navigation-compose:2.5.2")
 }
